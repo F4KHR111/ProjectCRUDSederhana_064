b7be03f467be61f9afad49804aacf0e008cc9d7d
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-03-20 18:30:30 +0700

    Mengambil Email dari kolom ketiga dan menampilkannya di txtEmail.

--- Form1.cs
+++ Form1.cs
@@ -230,6 +230,7 @@ namespace ProjectCRUDSederhana
 
                 txtNIM.Text = row.Cells[0].Value.ToString();
                 txtNama.Text = row.Cells[1].Value.ToString();
+                txtEmail.Text = row.Cells[2].Value.ToString();
 
 
             }
